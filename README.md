62a05b302aba344cc14673af6a0793341e033f3b
Marcel Hinderlich <m.hinderlich@reply.de>
2023-01-13 17:27:09 +0100

    added further examples

--- README.md
+++ README.md
@@ -6,6 +6,5 @@ Collection created during svelte learning process
 
 - hello-svelte (default project created with ```npm init vite@latest``` Svelte, Javascript)
 - hello-sveltekit (default project created with ```npm create svelte@latest hello-sveltekit``` TS, ESLint, Prettier, Playwright, Vitest )
-
-
 - svelte-tutorial (https://www.udemy.com/course/sveltejs-the-complete-guide/)
+- svelte-todo (simple todo list with vite)
---
+++ vite-todo/src/App.svelte
@@ -0,0 +1,55 @@
+<script>
+  let input = "";
+  let todos = [
+    {
+      name: "Learn svelte",
+      checked: false,
+    },
+    {
+      name: "do the first project with svelte",
+      checked: false,
+    },
+    {
+      name: "Drink coffee",
+      checked: false,
+    },
+  ];
+
+  function createTodo() {
+    if (!input) return;
+    todos = [...todos, { name: input, checked: false }];
+    input = "";
+  }
+
+  function removeItem(index) {
+    // TODO
+    console.log("removeItem");
+    todos.splice(index, 1);
+    todos = todos;
+  }
+</script>
+
+<main>
+  <h1>Todo</h1>
+  <input type="text" bind:value={input} />
+  <button on:click={createTodo}>Create Todo</button>
+  <ul>
+    {#each todos as todo, index}
+      <li>
+        <input
+          type="checkbox"
+          bind:checked={todo.checked}
+          on:click={() => (todo.checked = !todo.checked)}
+        />
+        <span class:checked={todo.checked}>{todo.name}</span>
+        <span on:click={() => removeItem(index)}>❌</span>
+      </li>
+    {/each}
+  </ul>
+</main>
+
+<style>
+  .checked {
+    text-decoration: line-through;
+  }
+</style>
